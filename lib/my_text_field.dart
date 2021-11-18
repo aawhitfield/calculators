e3105cd231797d8091eaced3d51233b9d6cc5c89
Aaron Whitfield <aawhitfield@hotmail.com>
2021-11-17 17:13:20 -0800

    adding input formatters

--- lib/my_text_field.dart
+++ lib/my_text_field.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MyTextField extends StatelessWidget {
   final String? labelText;
   final Function(String)? onChanged;
+  final List<TextInputFormatter>? inputFormatters;
 
   const MyTextField({
     this.labelText,
     this.onChanged,
+    this.inputFormatters,
     Key? key,
   }) : super(key: key);
 
@@ -15,8 +18,10 @@ class MyTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
+        inputFormatters: inputFormatters,
         decoration: InputDecoration(
           labelText: labelText,
+          labelStyle: TextStyle(color: Colors.green.shade900),
           border: const OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(12)),
           ),
