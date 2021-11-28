21d5aecb0c3ab635037a8d562f7958ebaa9a3c44
Aaron Whitfield <aawhitfield@hotmail.com>
2021-11-28 09:40:32 -0800

    creates placeholder screens for finance option down payment and construction loans

---
+++ lib/inputs/finance_option_down_payment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FinanceOptionDownPayment extends StatelessWidget {
+  const FinanceOptionDownPayment({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
